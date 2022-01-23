8c6b67147eded048a078906e1d78f3dc3438dbfa
shanmugavel <m.shanmugavel@gmail.com>
2022-01-24 00:17:31 +0530

    ci cd pipeline project

---
+++ src/main/java/com/shan/sample/ShanCICDPipeline/ShanCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.shan.sample.ShanCICDPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShanCicdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shan/sample/ShanCICDPipeline/ShanCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan.sample.ShanCICDPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShanCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
